97a825d5ce98adaad043ab1c2959454240f426e8
B. Scott Michel <scooter.phd@gmail.com>
2019-08-18 09:06:19 -0400

    finish add_simulator upgrade, BuildROMs
    Add BuildROMs support, with the DONT_USE_ROMS option.

--- cmake/add_simulator.cmake
+++ cmake/add_simulator.cmake
@@ -141,7 +141,7 @@ endfunction(build_simcore _targ)
 ## FULL64: Use the simhz64 library (USE_INT64, USE_ADDR64)
 
 function (add_simulator _targ) ## _sources _defines _includes)
-    cmake_parse_arguments(SIMH "INT64;FULL64" "TEST" "DEFINES;INCLUDES;SOURCES" ${ARGN})
+    cmake_parse_arguments(SIMH "INT64;FULL64;BUILDROMS" "TEST" "DEFINES;INCLUDES;SOURCES" ${ARGN})
 
     if (NOT DEFINED SIMH_SOURCES)
         message(FATAL_ERROR "${_targ}: No source files?")
@@ -183,6 +183,10 @@ function (add_simulator _targ) ## _sources _defines _includes)
     if (DEFINED SIMH_TEST AND EXISTS "${test_fname}")
 	add_test(NAME "test-${_targ}" COMMAND "${_targ}" "${test_fname}")
     endif (DEFINED SIMH_TEST AND EXISTS "${test_fname}")
+
+    if (NOT DONT_USE_ROMS AND SIMH_BUILDROMS)
+	add_dependencies(${_targ} BuildROMs)
+    endif (NOT DONT_USE_ROMS AND SIMH_BUILDROMS)
 endfunction ()
 
 ##~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
@@ -192,3 +196,45 @@ endfunction ()
 build_simcore(simhcore)
 build_simcore(simhi64 DEFINES USE_INT64)
 build_simcore(simhz64 DEFINES USE_INT64 USE_ADDR64)
+
+if (NOT DONT_USE_ROMS)
+    add_executable(BuildROMs sim_BuildROMs.c)
+    add_custom_command(
+	OUTPUT
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka655x_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka620_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka630_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka610_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka410_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka411_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka412_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka41a_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka41d_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka42a_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka42b_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka43a_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka46a_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka47a_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka48a_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_is1000_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka410_xs_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka420_rdrz_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka420_rzrz_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka4xx_4pln_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka4xx_8pln_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka4xx_dz_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka4xx_spx_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka750_bin_new.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_ka750_bin_old.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_vcb02_bin.h
+	    ${CMAKE_SOURCE_DIR}/VAX/vax_vmb_exe.h
+	    ${CMAKE_SOURCE_DIR}/PDP11/pdp11_vt_lunar_rom.h
+	    ${CMAKE_SOURCE_DIR}/PDP11/pdp11_dazzle_dart_rom.h
+	    ${CMAKE_SOURCE_DIR}/PDP11/pdp11_11logo_rom.h
+	    ${CMAKE_SOURCE_DIR}/swtp6800/swtp6800/swtp_swtbug_bin.h
+	    ${CMAKE_SOURCE_DIR}/3B2/rom_400_bin.h
+	MAIN_DEPENDENCY BuildROMs
+	COMMAND BuildROMS
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    )
+endif ()
