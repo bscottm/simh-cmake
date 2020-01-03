7a9e011d5413255a096b553e8bfbdbfe30665b97
B. Scott Michel <scooter.phd@gmail.com>
2020-01-03 11:54:25 -0800

    testonly option for cmake-builder.ps1
    Add the "-testonly" flag to cmake-builder clean up appveyor.yml.
    
    Add package directories to cache for appveyor builds -- should speed up
    the build process a fair bit.

--- cmake-builder.ps1
+++ cmake-builder.ps1
@@ -26,7 +26,8 @@ param (
     [switch] $nonetwork   = $false,
     [switch] $notest      = $false,
     [switch] $parallel    = $false,
-    [switch] $generate    = $false
+    [switch] $generate    = $false,
+    [switch] $testonly    = $false
 )
 
 $scriptName = $MyInvocation.InvocationName.replace('.\', '')
@@ -93,95 +94,98 @@ was installed.
     exit 1
 }
 
-## bison/winbison and flex/winflex: Just make sure they exist.
-if (!$nonetwork) {
-  $haveBison = $false
-  $haveFlex  = $false
-  foreach ($i in @("bison", "winbison")) {
-    if ($(Get-Command $i -ErrorAction Ignore).Path.Length -gt 0) { $haveBison = $true }
-  }
-
-  foreach ($i in @("flex",  "winflex")) {
-    if ($(Get-Command $i -ErrorAction Ignore).Path.Length -gt 0) { $haveFlex  = $true }
-  }
-
-  if (!$haveBison)
-  {
-      @"
-  !! ${scriptName} error:
-
-  Did not find 'bison' or 'winbison'. Please ensure you have installed bison or
-  winbison and that your PATH environment variables references the directory in
-  which it was installed.
-"@
-  }
-
-  if (!$haveFlex)
-  {
-      @"
-  !! ${scriptName} error:
-
-  Did not find 'flex' or 'winflex'. Please ensure you have installed flex or
-  winflex and that your PATH environment variables references the directory in
-  which it was installed.
-"@
-  }
-
-  if (!$haveBison -or !$haveFlex)
-  {
-    exit 1
-  }
-}
-
-## Check for GCC and mingw32-make if user wants the mingw flavor build.
-if ($flavor -eq "mingw" -or $flavor -eq "ninja")
+if (!$testOnly)
 {
-    if ($(Get-Command gcc -ErrorAction Ignore).Path.Length -eq 0) {
-        @"
-!! ${scriptName} error:
+    ## bison/winbison and flex/winflex: Just make sure they exist.
+    if (!$nonetwork) {
+      $haveBison = $false
+      $haveFlex  = $false
+      foreach ($i in @("bison", "winbison")) {
+	if ($(Get-Command $i -ErrorAction Ignore).Path.Length -gt 0) { $haveBison = $true }
+      }
 
-Did not find 'gcc', the GNU C/C++ compiler toolchain. Please ensure you have
-installed gcc and that your PATH environment variables references the directory
-in which it was installed.
+      foreach ($i in @("flex",  "winflex")) {
+	if ($(Get-Command $i -ErrorAction Ignore).Path.Length -gt 0) { $haveFlex  = $true }
+      }
+
+      if (!$haveBison)
+      {
+	  @"
+      !! ${scriptName} error:
+
+      Did not find 'bison' or 'winbison'. Please ensure you have installed bison or
+      winbison and that your PATH environment variables references the directory in
+      which it was installed.
 "@
-        exit 1
+      }
+
+      if (!$haveFlex)
+      {
+	  @"
+      !! ${scriptName} error:
+
+      Did not find 'flex' or 'winflex'. Please ensure you have installed flex or
+      winflex and that your PATH environment variables references the directory in
+      which it was installed.
+"@
+      }
+
+      if (!$haveBison -or !$haveFlex)
+      {
+	exit 1
+      }
     }
 
-    if ($(Get-Command mingw32-make -ErrorAction Ignore).Path.Length -eq 0) {
-        @"
-!! ${scriptName} error:
+    ## Check for GCC and mingw32-make if user wants the mingw flavor build.
+    if ($flavor -eq "mingw" -or $flavor -eq "ninja")
+    {
+	if ($(Get-Command gcc -ErrorAction Ignore).Path.Length -eq 0) {
+	    @"
+    !! ${scriptName} error:
 
-Did not find 'mingw32-make'. Please ensure you have installed mingw32-make and
-that your PATH environment variables references the directory in which it was
-installed.
-
-Note: 'mingw32-make' is part of the MinGW-W64 software ecosystem. You may need
-to open a MSYS or MinGW64 terminal window and use 'pacman' to install it.
-
-Alternatively, if you use the Scoop package manager, 'scoop install gcc'
-will install this as part of the current GCC compiler instalation.
+    Did not find 'gcc', the GNU C/C++ compiler toolchain. Please ensure you have
+    installed gcc and that your PATH environment variables references the directory
+    in which it was installed.
 "@
-        exit 1
+	    exit 1
+	}
+
+	if ($(Get-Command mingw32-make -ErrorAction Ignore).Path.Length -eq 0) {
+	    @"
+    !! ${scriptName} error:
+
+    Did not find 'mingw32-make'. Please ensure you have installed mingw32-make and
+    that your PATH environment variables references the directory in which it was
+    installed.
+
+    Note: 'mingw32-make' is part of the MinGW-W64 software ecosystem. You may need
+    to open a MSYS or MinGW64 terminal window and use 'pacman' to install it.
+
+    Alternatively, if you use the Scoop package manager, 'scoop install gcc'
+    will install this as part of the current GCC compiler instalation.
+"@
+	    exit 1
+	}
     }
-}
 
-if (!$nonetwork -and !(Test-Path -Path "C:\Windows\System32\Npcap"))
-{
-    ## Note: Windows does redirection under the covers to make the 32-bit
-    ## version of Npcap appear in the C:\Windows\System32 directory. But
-    ## it should be sufficient to detect the 64-bit runtime because both
-    ## get installed.
-    @"
-!! ${scriptName} error:
+    if (!$nonetwork -and !(Test-Path -Path "C:\Windows\System32\Npcap"))
+    {
+	## Note: Windows does redirection under the covers to make the 32-bit
+	## version of Npcap appear in the C:\Windows\System32 directory. But
+	## it should be sufficient to detect the 64-bit runtime because both
+	## get installed.
+	@"
+    !! ${scriptName} error:
 
-Did not find the Npcap packet capture runtime. Please install it for simulator
-network support:
+    Did not find the Npcap packet capture runtime. Please install it for simulator
+    network support:
 
-    https://nmap.org/npcap/
+	https://nmap.org/npcap/
 
-Or invoke this script with the "-nonetwork" flag to disable networking support.
+    Or invoke this script with the "-nonetwork" flag to disable networking support.
 
 "@
+    }
 }
 
 ## Look for Git's /usr/bin subdirectory: CMake (and other utilities) have issues
@@ -195,7 +199,8 @@ if ($tmp_path -ne ${env:PATH})
     "** ${scriptName}: Removed ${git_usrbin} from PATH (Git MinGW problem)"
     $env:PATH = $tmp_path
 }
-#
+
+
 ## Setup:
 $buildDir  = "cmake-vs"
 $generator = "!!invalid!!"
@@ -204,29 +209,29 @@ $archFlag  = @()
 switch ($flavor)
 {
     "2019" {
-        $buildDir += $flavor
-        $generator = "Visual Studio 16 2019"
-        $archFlag  = @("-A", "Win32")
+	$buildDir += $flavor
+	$generator = "Visual Studio 16 2019"
+	$archFlag  = @("-A", "Win32")
     }
     "2017" {
-        $buildDir += $flavor
-        $generator = "Visual Studio 15 2017"
+	$buildDir += $flavor
+	$generator = "Visual Studio 15 2017"
     }
     "2015" {
-        $buildDir += $flavor
-        $generator = "Visual Studio 14 2015"
+	$buildDir += $flavor
+	$generator = "Visual Studio 14 2015"
     }
     "2013" {
-        $buildDir += $flavor
-        $generator = "Visual Studio 12 2013"
+	$buildDir += $flavor
+	$generator = "Visual Studio 12 2013"
     }
     "2012" {
-        $buildDir += $flavor
-        $generator = "Visual Studio 11 2012"
+	$buildDir += $flavor
+	$generator = "Visual Studio 11 2012"
     }
     "mingw" {
-        $buildDir = "cmake-mingw"
-        $generator = "MinGW Makefiles"
+	$buildDir = "cmake-mingw"
+	$generator = "MinGW Makefiles"
     }
     "ninja" {
 	$buildDir = "cmake-ninja"
@@ -237,97 +242,120 @@ switch ($flavor)
 	Write-Output "!! ${scriptName}: Unrecognized build flavor '${flavor}'."
 	Write-Error  "!! ${scriptName}: Unrecognized build flavor '${flavor}'."
 	Write-Output ""
-        Show-Help
+	Show-Help
     }
 }
 
-if (!@("Release", "Debug").Contains($config))
+if (!$testonly)
 {
-    @"
-${scriptName}: Invalid configuration: "${config}".
+    if (!@("Release", "Debug").Contains($config))
+    {
+	@"
+    ${scriptName}: Invalid configuration: "${config}".
 
 "@
-    Show-Help
-}
+	Show-Help
+    }
 
-## Clean out the 
-if ((Test-Path -Path ${buildDir}) -and $clean)
-{
-    "** ${scriptName}: Removing ${buildDir}"
-    Remove-Item -recurse -force -Path ${buildDir} -ErrorAction Continue | Out-Null
-}
-
-if (!(Test-Path -Path ${buildDir}))
-{
-    "** ${scriptName}: Creating ${buildDir} subdirectory"
-    New-Item -Path ${buildDir} -ItemType Directory | Out-Null
-}
-else
-{
-    "** ${scriptName}: ${buildDir} exists."
-}
-
-## Where we do the heaving lifting:
-$generateArgs = @("-G", ${generator}, "-D", "CMAKE_BUILD_TYPE=${config}") + ${archFlag} + @("..")
-if ($nonetwork)
-{
-    $generateArgs += @("-DWITH_NETWORK:Bool=Off", "-DWITH_PCAP:Bool=Off", "-DWITH_SLIRP:Bool=Off")
-}
-$generateArgs += @("..")
-
-$buildArgs     =  @("--build", ".", "--config", "${config}")
-if ($parallel)
-{
-  $buildArgs += "--parallel"
-}
-
-$buildSpecificArgs = @()
-if ($flavor -eq "mingw")
-{
-  ## Limit the number of parallel jobs mingw32-make can spawn. Otherwise
-  ## it'll overwhelm the machine.
-  $buildSpecificArgs += @("-j",  "8")
-}
-
-$exitval = 0
-
-Push-Location ${buildDir}
-try
-{
-    "** ${scriptName}: Configuring and generating"
-
-    & ${cmakeCmd} ${generateArgs} 2>&1
-    if (!$generate)
+    ## Clean out the 
+    if ((Test-Path -Path ${buildDir}) -and $clean)
     {
-	"** ${scriptName}: Building simulators."
-	& ${cmakeCmd} ${buildArgs} -- ${buildSpecificArgs}
-	if (!$notest)
-	{
-	  ## Let's test our results...
-	  ##
-	  ## If cmake failed, ctest will also fail. That's OK.
-	  ##
-	  ## Note: We're in the build directory already, so we can prepend $(Get-Location) for the
-	  ## full path to the build directory, then normalize it.
-	  $currentPath = $env:PATH
-	  $buildStageBin = [System.IO.Path]::GetFullPath("$(Get-Location)\build-stage\bin")
-	  $env:PATH =  "${buildStageBin};C:\Windows\System32\Npcap;${env:PATH}"
-	  & $ctestCmd @("-C", $config)
-	  $env:PATH = $currentPath
+	"** ${scriptName}: Removing ${buildDir}"
+	Remove-Item -recurse -force -Path ${buildDir} -ErrorAction Continue | Out-Null
+    }
+
+    if (!(Test-Path -Path ${buildDir}))
+    {
+	"** ${scriptName}: Creating ${buildDir} subdirectory"
+	New-Item -Path ${buildDir} -ItemType Directory | Out-Null
+    }
+    else
+    {
+	"** ${scriptName}: ${buildDir} exists."
+    }
+
+    ## Where we do the heaving lifting:
+    $generateArgs = @("-G", ${generator}, "-D", "CMAKE_BUILD_TYPE=${config}") + ${archFlag} + @("..")
+    if ($nonetwork)
+    {
+	$generateArgs += @("-DWITH_NETWORK:Bool=Off", "-DWITH_PCAP:Bool=Off", "-DWITH_SLIRP:Bool=Off")
+    }
+    $generateArgs += @("..")
+
+    $buildArgs     =  @("--build", ".", "--config", "${config}")
+    if ($parallel)
+    {
+      $buildArgs += "--parallel"
+    }
+
+    $buildSpecificArgs = @()
+    if ($flavor -eq "mingw")
+    {
+      ## Limit the number of parallel jobs mingw32-make can spawn. Otherwise
+      ## it'll overwhelm the machine.
+      $buildSpecificArgs += @("-j",  "8")
+    }
+
+    $exitval = 0
+
+    Push-Location ${buildDir}
+    try
+    {
+	"** ${scriptName}: Configuring and generating"
+
+	if (!$testonly) {
+	    & ${cmakeCmd} ${generateArgs} 2>&1
+	    if (!$generate)
+	    {
+		"** ${scriptName}: Building simulators."
+		& ${cmakeCmd} ${buildArgs} -- ${buildSpecificArgs}
+	    }
+	    else
+	    {
+		"** ${scriptName}: Generated build environment in $(Get-Location)"
+		exit 0
+	    }
 	}
-    } else {
-	"** ${scriptName}: Generated build environment in $(Get-Location)"
+    }
+    catch
+    {
+	"** ${scriptName}: Caught error, exiting."
+	Format-List * -force -InputObject $_
+	$exitval = 1
+    }
+    finally
+    {
+	Pop-Location
     }
 }
-catch
+
+if (!$notest)
 {
-    "** ${scriptName}: Caught error, exiting."
-    Format-List * -force -InputObject $_
-    $exitval = 1
-}
-finally
-{
-    Pop-Location
+    try {
+	## Let's test our results...
+	##
+	## If cmake failed, ctest will also fail. That's OK.
+	##
+	## Note: We're in the build directory already, so we can prepend $(Get-Location) for the
+	## full path to the build directory, then normalize it.
+	Push-Location ${buildDir}
+
+	$currentPath = $env:PATH
+	$buildStageBin = [System.IO.Path]::GetFullPath("$(Get-Location)\build-stage\bin")
+	$env:PATH =  "${buildStageBin};C:\Windows\System32\Npcap;${env:PATH}"
+	& $ctestCmd @("-C", $config)
+	$env:PATH = $currentPath
+    }
+    catch
+    {
+	"** ${scriptName}: Caught error, exiting."
+	Format-List * -force -InputObject $_
+	$exitval = 1
+    }
+    finally
+    {
+	Pop-Location
+    }
 }
 
 exit $exitval
